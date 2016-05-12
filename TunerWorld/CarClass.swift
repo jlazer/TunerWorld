0adba20fff12c62be4628b394bc7b1b8ef8b87ae
student1 <student1@jh-mbair-158-06.d214.org>
2016-05-12 15:26:44 -0500

    Pasted in the correct code for getting all 7811 cars in the Edmunds database.

---
+++ TunerWorld/CarClass.swift
@@ -0,0 +1,49 @@
+//
+//  CarClass.swift
+//  TunerWorld
+//
+//  Created by student1 on 5/12/16.
+//  Copyright © 2016 JohnHerseyHighSchool. All rights reserved.
+//
+
+import Foundation
+
+class Car
+{
+    var make: String
+    var model: String
+    var year: NSNumber
+    var engine: String
+    var transmission: String
+    
+    init(make mk: String, model md: String, year y: NSNumber)
+    {
+        make = mk
+        model = md
+        year = y
+        engine = ""
+        transmission = ""
+    }
+    
+    
+    
+    init(make mk: String, model md: String, year y: NSNumber, engine e: String, transmission t: String)
+    {
+        make = mk
+        model = md
+        year = y
+        engine = e
+        transmission = t
+        
+    }
+    
+    init ()
+    {
+        make = ""
+        model = ""
+        year = NSNumber()
+        engine = ""
+        transmission = ""
+    }
+    
+}
--- TunerWorld/ViewController.swift
+++ TunerWorld/ViewController.swift
@@ -10,13 +10,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let requestURL: NSURL = NSURL(string: "https://api.edmunds.com/api/vehicle/v2/makes?fmt=json&api_key=rduby6uckm74q7f3jy72x344")!
+        let urlRequest: NSMutableURLRequest = NSMutableURLRequest(URL: requestURL)
+        let session = NSURLSession.sharedSession()
+        var listOfMakes = NSArray()
+//        var arrayOfMakes = [String]()
+//        var arrayOfModels = [String]()
+        var arrayOfCars = [Car]()
+        
+        let task = session.dataTaskWithRequest(urlRequest) {
+            (data, response, error) -> Void in
+            
+            let httpResponse = response as! NSHTTPURLResponse
+            let statusCode = httpResponse.statusCode
+            
+            if (statusCode == 200) {
+                do {
+                    let results = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+                    listOfMakes = results.objectForKey("makes") as! NSArray
+                    
+                    
+                    var make = ""
+                    var model = ""
+                    var year: NSNumber!
+                    
+                    
+                    for thing1 in listOfMakes
+                    {
+                        make = thing1.objectForKey("name") as! String
+                        
+                        for thing2 in thing1.objectForKey("models") as! NSArray
+                        {
+                            model = thing2.objectForKey("name") as! String
+                            
+                            for thing3 in thing2.objectForKey("years") as! NSArray
+                            {
+                                year = thing3.objectForKey("year") as! NSNumber
+//                                print("Make: \(make) Model: \(model) Year: \(year)")
+                                //Car(make: make, model: model, year: year)
+                                arrayOfCars.append(Car(make: make, model: model, year: year))
+                                
+                                
+                            }
+                        }
+                    }
+                    print(arrayOfCars.count)
+                    print(arrayOfCars[32].make)
+                    
+                    
+                } catch {
+                    print("error serializing JSON: \(error)")
+                }
+            }
+        }
+        
+        task.resume()
+        
+        
+        
     }
-
     
-
+    
+    
 }
 
