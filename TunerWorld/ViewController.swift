96a4d85d586b56035fabc7578c407b2931a6542b
student1 <student1@jh-mbair-158-06.d214.org>
2016-05-13 09:44:44 -0500

    The first tableView is displaying the makes.

--- TunerWorld/ViewController.swift
+++ TunerWorld/ViewController.swift
@@ -9,18 +9,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var arrayOfCars = [Car]()
+    var arrayOfMakes = [String]()
+    @IBOutlet weak var makeTableView: UITableView!
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
         let requestURL: NSURL = NSURL(string: "https://api.edmunds.com/api/vehicle/v2/makes?fmt=json&api_key=rduby6uckm74q7f3jy72x344")!
         let urlRequest: NSMutableURLRequest = NSMutableURLRequest(URL: requestURL)
         let session = NSURLSession.sharedSession()
         var listOfMakes = NSArray()
-//        var arrayOfMakes = [String]()
-//        var arrayOfModels = [String]()
-        var arrayOfCars = [Car]()
+        //        var arrayOfMakes = [String]()
+        //        var arrayOfModels = [String]()
         
         let task = session.dataTaskWithRequest(urlRequest) {
             (data, response, error) -> Void in
@@ -42,6 +45,7 @@ class ViewController: UIViewController {
                     for thing1 in listOfMakes
                     {
                         make = thing1.objectForKey("name") as! String
+                        self.arrayOfMakes.append(make)
                         
                         for thing2 in thing1.objectForKey("models") as! NSArray
                         {
@@ -50,30 +54,43 @@ class ViewController: UIViewController {
                             for thing3 in thing2.objectForKey("years") as! NSArray
                             {
                                 year = thing3.objectForKey("year") as! NSNumber
-//                                print("Make: \(make) Model: \(model) Year: \(year)")
+                                //print("Make: \(make) Model: \(model) Year: \(year)")
                                 //Car(make: make, model: model, year: year)
-                                arrayOfCars.append(Car(make: make, model: model, year: year))
+                                self.arrayOfCars.append(Car(make: make, model: model, year: year))
                                 
                                 
                             }
                         }
                     }
-                    print(arrayOfCars.count)
-                    print(arrayOfCars[32].make)
+                    print(self.arrayOfCars.count)
+                    //print(self.arrayOfCars[32].make)
                     
                     
                 } catch {
                     print("error serializing JSON: \(error)")
                 }
             }
+            self.makeTableView.reloadData()
         }
         
         task.resume()
         
         
+
         
     }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("myCell")!
+        cell.textLabel?.text = arrayOfMakes[indexPath.row]
+        return cell
+    }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayOfMakes.count
+    }
+    
+
     
     
 }
